2f2b7eedf1bc7ab4818080d97245d0a532e495bb
Jaron Whipple <jaronwhipple@byui.edu>
2022-03-08 02:56:30 -0700

    Testing branch

--- models/tag.js
+++ models/tag.js
@@ -5,7 +5,10 @@ const Schema = mongoose.Schema;
 const tagSchema = new Schema({
   tag: {
     name: String 
-  }, 
+  },
+  test: {
+    test: String
+  }
 });
 
 module.exports = mongoose.model('Tag', tagSchema);
