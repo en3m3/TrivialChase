273e53f1f9f00d0bbd416c3aa7b3c61f901a86b4
erikqb3 <erikqb3@gmail.com>
2022-03-09 19:01:07 -0700

    changes to user stuff

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,25 @@
+exports.getAllUsers = (req, res, next) => {
+    res.status(200).json({
+        posts: [{ 
+            title: 'user', 
+            // content: 'This is the user endpoint' }]
+            content: req.userId }]
+    });
+};
+
 exports.getUser = (req, res, next) => {
     res.status(200).json({
         posts: [{ title: 'user', content: 'This is the user endpoint' }]
     });
 };
 
-exports.postUser = (req, res, next) => {
+exports.postCreateUser = (req, res, next) => {
     res.status(200).json({
         posts: [{ title: 'user', content: 'This is the user endpoint' }]
     });
 };
 
-exports.putUser = (req, res, next) => {
+exports.putUpdateUser = (req, res, next) => {
     res.status(200).json({
         posts: [{ title: 'user', content: 'This is the user endpoint' }]
     });
@@ -21,3 +30,70 @@ exports.deleteUser = (req, res, next) => {
         posts: [{ title: 'user', content: 'This is the user endpoint' }]
     });
 };
+
+
+
+exports.getLogin = (req, res, next) => {
+    res.status(200).json({
+        posts: [{ title: 'user', content: 'This is the user endpoint' }]
+    });
+};
+
+exports.getSignup = (req, res, next) => {
+    res.status(200).json({
+        posts: [{ title: 'user', content: 'This is the user endpoint' }]
+    });
+};
+
+exports.getReset = (req, res, next) => {
+    res.status(200).json({
+        posts: [{ title: 'user', content: 'This is the user endpoint' }]
+    });
+};
+
+exports.getNewPassword = (req, res, next) => {
+    res.status(200).json({
+        posts: [{ title: 'user', content: 'This is the user endpoint' }]
+    });
+};
+
+
+
+exports.postLogin = (req, res, next) => {
+    res.status(200).json({
+        posts: [{ title: 'user', content: 'This is the user endpoint' }]
+    });
+};
+
+
+
+
+
+
+
+
+// //Starter Stuff provided by Martin
+
+// xports.getUser = (req, res, next) => {
+//     res.status(200).json({
+//         posts: [{ title: 'user', content: 'This is the user endpoint' }]
+//     });
+// };
+
+// exports.postUser = (req, res, next) => {
+//     res.status(200).json({
+//         posts: [{ title: 'user', content: 'This is the user endpoint' }]
+//     });
+// };
+
+// exports.putUser = (req, res, next) => {
+//     res.status(200).json({
+//         posts: [{ title: 'user', content: 'This is the user endpoint' }]
+//     });
+// };
+
+// exports.deleteUser = (req, res, next) => {
+//     res.status(200).json({
+//         posts: [{ title: 'user', content: 'This is the user endpoint' }]
+//     });
+// };
--- models/user.js
+++ models/user.js
@@ -16,3 +16,26 @@ const userSchema = new Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
+
+
+
+// // Starter Stuff provided by Martin
+
+// const mongoose = require('mongoose');
+
+// const Schema = mongoose.Schema;
+
+// const userSchema = new Schema({
+//   email: {
+//     type: String,
+//     required: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   resetToken: String,
+//   resetTokenExpiration: Date,
+// });
+
+// module.exports = mongoose.model('User', userSchema);
