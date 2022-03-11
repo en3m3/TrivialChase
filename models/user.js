b5f1333822c58df62457cf171f83896d8f40a54f
erikqb3 <erikqb3@gmail.com>
2022-03-11 11:12:44 -0700

    Finished User model and methods

--- models/user.js
+++ models/user.js
@@ -17,8 +17,8 @@ const userSchema = new Schema({
   },
   resetToken: String,
   resetTokenExpiration: Date,
-  collection: { //collection of quiz sets
-    sets: [ //quiz sets
+  collection: { //collection of quiz sets created and played; similar to Cart
+    quizzes: [ //an array of quiz objects
       {
       quizId: {
         type: Schema.Types.ObjectId,
@@ -35,24 +35,80 @@ const userSchema = new Schema({
       }
     }
     ]
-  },
-
-  score: {
-    type: Int,
-    required: true
-  },
-
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
 
-userSchema.methods.createQuiz = function(quiz){
-  const collectionQuizsetIndex = this.collection.sets.findIndex(cb => { //cb = callback? cp from ecomerse
-    return cb.quizId.toString() === quiz._id.toString
+userSchema.methods.createQuiz = function(quiz) {
+  //STEP1 create array of current quizzes
+  //STEP2 push parameter quiz into created array
+  //STEP3 make value of collection equal to new created array
+  //STEP4 save
+
+  const updatedQuizzes = [...this.collection.quizzes];//STEP1
+  //STEP2
+  updatedQuizzes.push({
+    quizId: quiz.id,
+    created: true,
+    score: null
   })
+  //STEP3
+  const updatedCollection = {
+    quizzes: updatedQuizzes
+  }
+  this.collection = updatedCollection;
+  return this.save();//STEP4
 }
 
-userSchema.methods.deleteQuiz = function(){}
+userSchema.methods.addQuiz = function(quiz) { //add like to favorites
+  //STEP1 create array of current quizzes
+  //STEP2 push parameter quiz into created array
+  //STEP3 make value of collection equal to new created array
+  //STEP4 save
+
+  const updatedQuizzes = [...this.collection.quizzes];//STEP1
+  //STEP2
+  updatedQuizzes.push({
+    quizId: quiz.id,
+    created: false, //different, 
+    score: quiz.score
+  })
+  //STEP3
+  const updatedCollection = {
+    quizzes: updatedQuizzes
+  }
+  this.collection = updatedCollection;
+  return this.save();//STEP4
+}
+
+userSchema.methods.deleteQuiz = function(quiz){
+  //STEP1 create array of current quizzes
+  //STEP2 loop through array and see if quiz from parameter matches any current quizzes
+  //STEP3 make value of collection equal to new created array
+  //STEP4 save
+
+  const updatedQuizzes = [...this.collection.quizzes];//STEP1
+  //STEP2
+  for (let i=0; i<updatedQuizzes.length; i++) { 
+    if (updatedQuizzes[i].quizId == quiz.id) {
+      updatedQuizzes.splice(i,1) //at position i, take out 1 (self)
+    }
+  }
+  //STEP3
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCart;
+  return this.save();//STEP4
+}
+
+
+
+
+
+
+
 
 // // Starter Stuff provided by Martin
 
