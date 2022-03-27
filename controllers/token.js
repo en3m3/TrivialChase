1c1bbcd7185beb60b0137f00b43a33e00fc69e84
“Foxx-Skyler” <foxxskyler@gmail.com>
2022-03-26 17:09:04 -0700

    tokenCommit

--- controllers/token.js
+++ controllers/token.js
@@ -1,19 +1,25 @@
+const mongoose = require('mongoose');
+
 exports.getSessionToken = (req, res, next) => {
     console.log("here");
+    req.session.getSessionToken;
     res.status(200).json({
         posts: [{ title: 'token', content: 'This is the token endpoint' }]
     });
 };
 
 exports.postSessionToken = (req, res, next) => {
+    req.session.postSessionToken;
     res.status(200).json({
         posts: [{ title: 'token', content: 'This is the token endpoint' }]
     });
 };
 
 exports.deleteSessionToken = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+    });
     res.status(200).json({
         posts: [{ title: 'token', content: 'This is the token endpoint' }]
     });
 };
-
