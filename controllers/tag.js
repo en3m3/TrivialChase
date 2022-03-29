32cefc5801f16ad16d1acf704d9265e8f80d5b3c
Jaron Whipple <jaronwhipple@byui.edu>
2022-03-28 18:07:54 -0600

    tag model updates

--- controllers/tag.js
+++ controllers/tag.js
@@ -1,21 +1,33 @@
+const mongoose = require('mongoose');
+const Tag = require('../models/tag');
+
 exports.getAllTags = (req, res, next) => {
-    console.log("here");
-    res.status(200).json({
-        posts: [{ title: 'tag', content: 'This is the tag endpoint' }]
+    Tag.find().then(tag => {
+        console.log(tag);
+        res.status(200).json({
+            tag: tag })
     });
 };
 
 exports.getTag = (req, res, next) => {
-    console.log("here");
-    res.status(200).json({
-        posts: [{ title: 'tag', content: 'This is the tag endpoint' }]
-    });
+    const tag = Tag.findById(req.params.id)
+    .then(tag => res.status(200).json(tag))
+    .then(console.log('tag found by ID'))
+    .catch(err => res.json(err));
 };
 
 exports.postTag = (req, res, next) => {
-    res.status(200).json({
-        posts: [{ title: 'tag', content: 'This is the tag endpoint' }]
-    });
+    const tag = new Tag({
+        tag_id: req.body.id,
+        tag: req.body.tag
+    })
+    tag.save().
+    then(data => {
+        res.status(200).json(data);
+    }).catch(err => {
+        res.json(err);
+    })
+    ;
 };
 
 exports.putTag = (req, res, next) => {
@@ -25,7 +37,8 @@ exports.putTag = (req, res, next) => {
 };
 
 exports.deleteTag = (req, res, next) => {
-    res.status(200).json({
-        posts: [{ title: 'tag', content: 'This is the tag endpoint' }]
-    });
+    Tag.deleteOne({_id: req.params.id})
+    .then(res.status(200).json({message: 'Removed Successfully'}))
+    .then(console.log('Tag removed successfully'))
+    .catch(err => res.json(err));
 };
