d88ffca72602afabf9a8b9024c37bb6a213ee4c1
MMelerio <martin.melerio@gmail.com>
2022-03-30 23:05:32 -0300

    Fixed score controller, model and routes

--- controllers/score.js
+++ controllers/score.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Score = require('../models/score');
-const HighScores = require('../models/score');
 const Quiz = require('../models/quiz');
 const User = require('../models/user');
 
@@ -24,36 +23,28 @@ exports.getScoreByUser = (req, res, next) => {
     .catch(err => res.json(err));
 };
 
-exports.getHighScores = (req, res, next) => { //GET 1 high score or all High Scores
-  const highscore = Score.find({score: req.params.user_id >=  75})
+exports.getHighScoresByQuiz = (req, res, next) => { //GET 1 high score or all High Scores
+  const highscore = Score.find({quiz: req.params.quiz_id}).sort({score: -1}).limit(1)
   .then(highscore => res.status(200).json(highscore))
   .then(console.log('High Scores retrieved sucessfully'))
   .catch(err => res.json(err));
 };
 
-exports.postHighScores = (req, res, next) => {
-  const scor = new HighScores({
-    quizId: req.body.quizId,
-    score: req.body.score,
-    date: req.body.date,
-    user_id: req.body.user_id,
-    quiz_id: req.body.quiz_id
-});
-scor.save().
-then(data => {
-    res.status(200).json(data);
-}).catch(err => {
-    res.json(err);
-});
+exports.getHighScoresByUser = (req, res, next) => { //GET 1 high score or all High Scores
+  const highscore = Score.find({user: req.params.user_id}).sort({score: -1}).limit(1)
+  .then(highscore => res.status(200).json(highscore))
+  .then(console.log('High Scores retrieved sucessfully'))
+  .catch(err => res.json(err));
 };
 
 exports.postScore = (req, res, next) => {
   const scor = new Score({
-    quizId: req.body.quizId,
     score: req.body.score,
     date: req.body.date,
-    user: req.body.user,
-    quiz: req.body.quiz
+    username: req.body.name,
+    quiz: req.body.quiz,
+    email: req.body.email,
+    user: req.body.user
 });
 scor.save().
 then(data => {
@@ -64,14 +55,14 @@ then(data => {
 };
 
 exports.putScore = (req, res, next) => {
-  Score.updateOne({quiz_id: req.params.quiz_id}, {$set: req.body})
+  Score.updateOne({_id: req.params.id}, {$set: req.body})
     .then(res.status(200).json({message: 'Updated Successfully'}))
     .then(console.log('Scores updated successfully'))
     .catch(err => res.json(err));
 };
 
 exports.deleteScore = (req, res, next) => {
-  Score.deleteOne({quiz_id: req.params.quiz_id})
+  Score.deleteOne({_id: req.params.id})
   .then(res.status(200).json({message: 'Deleted Successfully'}))
   .then(console.log('Scores deleted successfully'))
   .catch(err => res.json(err));
--- routes/score.js
+++ routes/score.js
@@ -11,20 +11,20 @@ router.get('/score/quiz/:quiz_id', scoreController.getScoreByQuiz);
 // GET all scores for a user
 router.get('/score/user/:user_id', scoreController.getScoreByUser);
 
-// GET user's high score(s?)
-router.get('/score/highscore/:user_id', scoreController.getHighScores);// 1 high score or all high scores?
+// GET quiz's high score(s?)
+router.get('/score/quiz/highscore/:quiz_id', scoreController.getHighScoresByQuiz);// 1 high score or all high scores?
 
-// POST create a new high scores linked to quiz
-router.post('/score/HighScores', scoreController.postHighScores);
+// GET user's high score(s?)
+router.get('/score/user/highscore/:user_id', scoreController.getHighScoresByUser);// 1 high score or all high scores?
 
 // POST create a new score linked to quiz
 router.post('/score/create', scoreController.postScore);
 
 // PUT update an existing score linked to quiz
-router.put('/score/update/:quiz_id', scoreController.putScore);
+router.put('/score/update/:id', scoreController.putScore);
 
 //DELETE a score linked to quiz
-router.delete('/score/delete/:quiz_id', scoreController.deleteScore);
+router.delete('/score/delete/:id', scoreController.deleteScore);
 
 
 module.exports = router;
