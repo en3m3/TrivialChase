2adeb979e3da78cbf6f3bdc341f1f0f76aaa105e
en3m3 <71306799+en3m3@users.noreply.github.com>
2022-03-30 20:49:41 -0600

    moved token to header

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,12 +1,12 @@
 const User = require('../models/user');
 
 module.exports = (req, res, next) => {
-    console.log(req.body);
-    if(req.body.token) {
-        User.findOne({token: req.body.token}).lean()
+    console.log(req.headers);
+    if(req.headers.token) {
+        User.findOne({token: req.headers.token})
         .then(user =>{
-            if(user) {
-                console.log("valid token or user");
+            if(user && user.tokenexpiration > Date.now()) {
+                console.log("valid token");
                 next();
             } else{
                 console.log("invalid token or user");
